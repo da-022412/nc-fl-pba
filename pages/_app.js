b62a3415c924c2bcb39a7fb9ede73b9d637ab291
Dennis Acosta <dennis@ferociousmedia.com>
2022-03-21 12:14:09 -0400

    Add layout components

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+
+import '../assets/styles/reset.css';
+
+import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <style global jsx>{`
+                :root {
+                    margin: 0;
+                    overflow-x: hidden;
+                    padding: 0;
+                    scroll-behavior: smooth;
+                }
+            `}</style>
+            <Head>
+                <title>North Central Florida PBA</title>
+            </Head>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </>
+    );
 }
 
-export default MyApp
+export default MyApp;
